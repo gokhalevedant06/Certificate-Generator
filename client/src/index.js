d6fffea99dff66ee4cfa095e3cdbc013e0ae2a34
Rewa Wader <raw.rewa10@gmail.com>
2021-10-06 15:14:55 +0530

    user-friendly UI changes

--- client/src/index.js
+++ client/src/index.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import Main from './Main';
+import App from './App';
 
 
 ReactDOM.render(
   <React.StrictMode>
-    <Main />
+    <App />
   </React.StrictMode>,
   document.getElementById('root')
 );
